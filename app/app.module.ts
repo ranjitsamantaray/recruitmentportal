6378ab09d1d9ec0a1f9d8ff9d4aa57e6a519f25f
Suraj <Suraj.nd4444@gmail.com>
2017-03-06 09:31:28 +0530

    Promoting without emp registration

--- app/app.module.ts
+++ app/app.module.ts
@@ -29,7 +29,7 @@ import { AuthGuard } from './auth.guard';
 import { ConfigService } from './config/config.service';
 import { HandleError,HandleErrorService } from './Services/HandleError.service';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import {EmpRegistrationComponent} from  './internal/empRegistration.component'
+//import {EmpRegistrationComponent} from  './internal/empRegistration.component'
 
 @NgModule ({
   imports: [ BrowserModule, routing, HttpModule, FormsModule,],
@@ -44,8 +44,8 @@ import {EmpRegistrationComponent} from  './internal/empRegistration.component'
                   TestSuccessComponent,
                   SummaryComponent,
                   EvaluationComponent,
-                  TimerComponent,
-                  EmpRegistrationComponent
+                  TimerComponent//,
+                  //EmpRegistrationComponent
                 ],  
   bootstrap: [ AppComponent ],
   providers: [{provide:SkillMethods, useClass:SkillService},
