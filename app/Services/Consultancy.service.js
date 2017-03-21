a478d150e5f3d0bfb8ba5f49d5df94cf610840c6
Ranjit Samantaray <ran@danskeit.co.in>
2017-03-21 09:23:16 +0530

    Added GlobalHunt Consultancy.service.js

--- app/Services/Consultancy.service.js
+++ app/Services/Consultancy.service.js
@@ -31,6 +31,7 @@ var ConsultancyService = (function () {
         this.con.push(new Consultancy_1.Consultancy(5, "Armour"));
         this.con.push(new Consultancy_1.Consultancy(6, "CareerNet"));
         this.con.push(new Consultancy_1.Consultancy(7, "Covenant India"));
+        this.con.push(new Consultancy_1.Consultancy(8, "GlobalHunt"));
         return Rs.Observable.from([this.con]);
         // return this._http.get(this.url)
         // .map((response : Response) => <Consultancy[]> response.json())
